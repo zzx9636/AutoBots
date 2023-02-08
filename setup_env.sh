8c150c1ef20ee4864eaf074db1efb22e9f938609
Zixu Zhang <zixuz@princeton.edu>
2023-02-08 13:35:00 -0500

    update set env script

--- setup_env.sh
+++ setup_env.sh
@@ -6,24 +6,23 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
+ENV_NAME=${1:-Autobots}    
+
 . /etc/profile
 module purge
 module load anaconda3/2022.10
 
 conda config --add channels conda-forge
-conda config --remove channels default
+conda config --add channels default
+conda config --set channel_priority flexible
 
 set -e
+echo -e "$BLUE Set up $RED$ENV_NAME$BLUE conda environment. $NC"
 
-echo -e "$BLUE Set up $RED$1$BLUE conda environment. $NC"
-
-exit 0
-conda env create -n $1 python=3.7
-conda activate $1
+conda create --name $ENV_NAME python=3.7 pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia --yes
+conda activate $ENV_NAME
 
 echo -e "$BLUE Install Dependencies. $NC"
-conda install -n $1 pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia --yes
-
 pip install h5py==3.3.0
 pip install opencv_python==4.5.3.56
 pip install pandas==1.3.1
@@ -36,14 +35,21 @@ pip install tensorboard
 
 # Set up dataset
 echo -e "$BLUD Install nuScenes devkit. $NC"
-mkdir third_parties
+mkdir -p third_parties
 cd third_parties
-git submodule add https://github.com/nutonomy/nuscenes-devkit.git
-
+if [ ! -d "nuscenes-devkit" ] 
+then
+    git submodule add https://github.com/nutonomy/nuscenes-devkit.git
+fi
 pip install nuscenes-devkit
 
 # echo -e "$BLUD Install nuScenes devkit. $NC"
 # git submodule add https://github.com/argoverse/argoverse-api.git
 # pip install -e argoverse-api
 
+echo -e "$BLUE Test Environment. $NC"
+cd ..
+python test_env.py
+
+echo -e "$GREEN Finished!\n To activate this environment, use: \n$RED$ conda activate test $NC"
 
